2c8547b2a89357d4dc05a56b32f91cff7053d0e6
Kate Summerill <kaitlynsummerill@gmail.com>
2020-01-27 02:31:50 +0000

    Automatically backed up by Learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -8,25 +8,11 @@ module Players
     # the move the computer wants to make in the form of a 1-9 string.
     # How the computer decides to make that move is up to you
     # but it must be capable of returning a valid move at some point.
-
-    # def x_o_count
-    #   x = @cells.count("X")
-    #   o = @cells.count("O")
-    # end
-
-    # inside #move, board = #<Board:0x0000000003364838 @cells=[" ", " ", " ", " ", " ", " ", " ", " ", " "]
-    # inside #move, @board and @cells are nil
-
     # ------ Strategy ------
     # if the computer goes first (token "X") it should choose the middle
     # if there's an O on the board, the computer should choose a place next to it
     # if the computer has 2 neighboring tokens on the board, it should choose the 3rd spot to win
     def move(board)
-      # binding.pry
-      # board.cells.turn
-      # x = @cells.count("X")
-      # o = @cells.count("O")
-      #
       turn_tracker = @board.turn_count
         # if the computer goes first (token "X") it should choose the middle
         if self.token == "X"
@@ -45,9 +31,6 @@ module Players
         end
       # def end
       end
-
-
-
 
     # class end
   end
